31b4e307f57487ee768c74a75b0d52c62d9ef266
shaman2507 <alabay174@gmail.com>
2023-05-09 21:51:21 +0200

    update

---
+++ js/01-gallery.js
@@ -0,0 +1,46 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+console.log(galleryItems);
+
+
+const galleryItemsEl = galleryItems.map(({ preview, original, description }) =>
+    `<li class="gallery_item">
+        <a class="gallery_link" href= "${original}">
+            <img
+            class="gallery_image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            width= 100%
+            />
+        </a>
+    </li>`
+).join('')
+
+const galleryEl = document.querySelector(".gallery")
+
+galleryEl.insertAdjacentHTML("beforeend", galleryItemsEl)
+
+galleryEl.addEventListener("click", addItemClassList)
+
+function addItemClassList(e) {
+    e.preventDefault()
+    if (e.target.classList.contains("gallery_image")) {
+        return;
+    }
+    console.log(e.target)
+}
+
+const instance = basicLightbox.create('<img src="${e.target.galleryEl}" width="800" height="600">',
+{
+    onShow: () => document.addEventListener("keydown", onCloseModal),
+    onClose: () => document.removeEventListener("keydown", onCloseModal),
+})
+
+instance.show();
+
+function onCloseModal(event) {
+    if (event.code === "Escape") {
+        instance.close();
+    }
+}
